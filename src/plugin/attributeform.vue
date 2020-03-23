81e1e787fa7b3cb5e3a386da0bc09bfbaa2477e6
毛永林 <1075039897@qq.com>
2020-03-23 22:28:32 +0800

    增加固定字段选择2

--- src/plugin/attributeform.vue
+++ src/plugin/attributeform.vue
@@ -9,7 +9,10 @@
           </el-form-item>
           <el-form-item v-if='form.showFrom.indexOf("key") !== -1 && fieldsdata.length > 0' label="字段名称">
             <el-select  style="width:100%" v-model="form.key" allow-create filterable>
-              <el-option  v-for='(item,index) in fieldsdata' :key="index" :label="item.label" :value="item.value"></el-option>
+              <el-option  v-for='(item,index) in fieldsdata' :key="index" :label="item.label" :value="item.value">
+                <span style="float: left">{{ item.label }}</span>
+                <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span>
+              </el-option>
             </el-select>
           </el-form-item>
           <el-form-item v-if='form.showFrom.indexOf("name") !== -1' label="中文名">
